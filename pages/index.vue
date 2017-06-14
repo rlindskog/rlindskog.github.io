124f134bb21975fc0ee00ecbd80a6f2742a9bf58
Ryan Lindskog <r.lindskog17@gmail.com>
2017-06-13 20:25:10 -0700

    updates to build scripts

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,7 @@
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <nuxt-link to="about" class="button--grey">about</nuxt-link>      
       </div>
     </div>
   </section>
